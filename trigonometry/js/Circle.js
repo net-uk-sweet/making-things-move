0b80761b87ca7f2c79225e04a95dcc0397a8d19b
net-uk-sweet <ian@sweet.uk.net>
2013-11-29 23:42:28 +0000

    Added instructions to all. Tidied up a few

--- trigonometry/js/Circle.js
+++ trigonometry/js/Circle.js
@@ -3,12 +3,16 @@
 
 function Circle(radius, color) {
 
-	this.radius = radius || 10;
-	this.color = color || '#fff';
+	radius = radius || 10;
+	color = color || '#fff';
 
 	// Could we extend shape?
 	var sprite = new createjs.Shape();
-	sprite.graphics.s(this.color).dc(0, 0, this.radius);
+	sprite.graphics.s(color).dc(0, 0, radius);
+
+	// This seems a pretty unsatisfactory way of doing this
+	// Easel has a getBounds method which doesn't seem to work
+	sprite.radius = radius;
 
 	return sprite;
 }
